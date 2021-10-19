1c2200979c7be7ca98c5bb48cba1e93fb487ec3d
Documenter.jl <documenter@juliadocs.github.io>
2021-10-19 16:59:38 +0000

    build based on 8d0f3d8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageJCM","category":"page"},{"location":"#DemoPackageJCM","page":"Home","title":"DemoPackageJCM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageJCM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageJCM]","category":"page"}]
+}
